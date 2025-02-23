13e20f8a4b0ce8c023a0e0ee359c73f8bb1e92f0
aaronJimenez <jimenezmontoya3@gmail.com>
2025-02-22 22:04:03 -0700

    updates
    updates

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+/*import { Component } from 'react'
 import './HeroImage.css'
 
 const styles = {
@@ -33,4 +33,43 @@ class HeroImage extends Component {
     }
 }
 
-export default HeroImage    
+export default HeroImage*/
+
+// HeroImage.js
+import React from 'react';
+import './HeroImage.css';
+
+const HeroImage = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-image">
+        <div className="hero-text">
+          <h2>
+            FREE COFFEE
+            <br />
+            IS A TAP AWAY
+          </h2>
+        </div>
+        <div className="hero-subtext">
+          <p>Join now to start earning Rewards.</p>
+        </div>
+        <div className="hero-container-2">
+          <button className="join-button">Join now</button>
+          <br />
+          <span>
+            Or{' '}
+            <a
+              href="https://www.starbucks.com/rewards/mobile-apps"
+              className="join-link"
+            >
+              join in the app
+            </a>{' '}
+            for the best experience
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImage;
